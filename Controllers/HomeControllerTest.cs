babceab2930bfd9ca6579b7fb1ba03bd5ed472e7
Luke Zeng <luke.zeng@zocdoc.com>
2015-01-07 11:56:43 -0500

    resharper refactor

--- Controllers/HomeControllerTest.cs
+++ Controllers/HomeControllerTest.cs
@@ -16,12 +16,13 @@ namespace MvvmSeed.Tests.Controllers
         public void Index()
         {
             // Arrange
-            HomeController controller = new HomeController();
+            var controller = new HomeController();
 
             // Act
-            ViewResult result = controller.Index() as ViewResult;
+            var result = controller.Index() as ViewResult;
 
             // Assert
+            Assert.IsNotNull(result);
             Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
         }
 
@@ -29,10 +30,10 @@ namespace MvvmSeed.Tests.Controllers
         public void About()
         {
             // Arrange
-            HomeController controller = new HomeController();
+            var controller = new HomeController();
 
             // Act
-            ViewResult result = controller.About() as ViewResult;
+            var result = controller.About() as ViewResult;
 
             // Assert
             Assert.IsNotNull(result);
@@ -42,10 +43,10 @@ namespace MvvmSeed.Tests.Controllers
         public void Contact()
         {
             // Arrange
-            HomeController controller = new HomeController();
+            var controller = new HomeController();
 
             // Act
-            ViewResult result = controller.Contact() as ViewResult;
+            var result = controller.Contact() as ViewResult;
 
             // Assert
             Assert.IsNotNull(result);
